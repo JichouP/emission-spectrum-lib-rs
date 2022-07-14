b435c79c4b95b3315af852dce150fbf520aa966e
JichouP <akkunn100@gmail.com>
2022-07-14 14:00:15 +0900

    :sparkles: add physical constants

---
+++ src/domain/constant.rs
@@ -0,0 +1,6 @@
+/// c (m/s)
+pub const SPEED_OF_LIGHT: f64 = 299_792_458.0;
+/// h (Js)
+pub const PLANCK_CONSTANT: f64 = 6.62607015e-34;
+/// e (C)
+pub const ELEMENTARY_CHARGE: f64 = 1.60217663e-19;
