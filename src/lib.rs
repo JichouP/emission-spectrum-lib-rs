35afb03ddaa369d61f202cdf3b7695bfafabbb75
JichouP <akkunn100@gmail.com>
2022-07-16 23:34:59 +0900

    :sparkles: add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
 pub mod domain;
+pub mod prelude;
 pub mod usecase;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use usecase::calc::*;
