05719defaf98c62cee58bd0c055e6fa0d41721a5
JichouP <akkunn100@gmail.com>
2022-07-16 23:48:06 +0900

    :construction: wip calc

---
+++ src/usecase/calc.rs
@@ -0,0 +1,10 @@
+use crate::prelude::*;
+
+#[derive(Debug, Clone)]
+pub struct CalcConfig {
+    spin_quantum: SpinQuantumNumber,
+}
+
+pub fn calc(config: CalcConfig) -> Vec<(f64, f64)> {
+    unimplemented!()
+}
