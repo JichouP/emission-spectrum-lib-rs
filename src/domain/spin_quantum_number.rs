49899b5158e1a00de22ef80a8693489dd829825c
JichouP <akkunn100@gmail.com>
2022-07-17 22:03:35 +0900

    :sparkles: Add BranchKind

--- src/domain/spin_quantum_number.rs
+++ src/domain/spin_quantum_number.rs
@@ -1,6 +1,56 @@
 #[derive(Debug, Clone)]
 pub enum SpinQuantumNumberKind {
-    Singlet,
-    Doublet,
-    Triplet,
+    Singlet(SingletBranchKind),
+    Doublet(DoubletBranchKind),
+    Triplet(TripletBranchKind),
+}
+
+#[derive(Debug, Clone)]
+pub enum SingletBranchKind {
+    P1,
+    Q1,
+    R1,
+}
+
+#[derive(Debug, Clone)]
+pub enum DoubletBranchKind {
+    P1,
+    Q1,
+    R1,
+    P2,
+    Q2,
+    R2,
+}
+
+#[derive(Debug, Clone)]
+pub enum TripletBranchKind {
+    P1,
+    Q1,
+    R1,
+    P2,
+    Q2,
+    R2,
+    P3,
+    Q3,
+    R3,
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::{DoubletBranchKind, SpinQuantumNumberKind};
+
+    #[test]
+    fn test() {
+        let kind = SpinQuantumNumberKind::Doublet(DoubletBranchKind::Q1);
+
+        match kind {
+            SpinQuantumNumberKind::Doublet(kind) => match kind {
+                DoubletBranchKind::Q1 => {
+                    assert!(true);
+                }
+                _ => unimplemented!(),
+            },
+            _ => unimplemented!(),
+        }
+    }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,7 @@
-pub use crate::domain::{spin_quantum_number::SpinQuantumNumberKind, term::Term};
+pub use crate::domain::{
+    spin_quantum_number::{
+        DoubletBranchKind, SingletBranchKind, SpinQuantumNumberKind, TripletBranchKind,
+    },
+    term::Term,
+};
 pub use crate::usecase::dunham_expansion::DunhamExpansion;
